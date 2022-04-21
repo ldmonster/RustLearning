55028d539e5bad81b95d43024596e55d91501a23
Paul <evilcorefriends@gmail.com>
2022-04-21 21:19:33 +0300

    init

--- src/bin/a7.rs
+++ src/bin/a7.rs
@@ -10,4 +10,47 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+use self::Colors::*;
+use std::slice::Iter;
+
+enum Colors {
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Orange,
+    Purple,
+}
+
+impl Colors {
+    pub fn iterator() -> Iter<'static, Colors> {
+        static COLORS: [Colors; 6] = [
+            Red,
+            Green,
+            Blue,
+            Yellow,
+            Orange,
+            Purple,
+        ];
+        COLORS.iter()
+    }
+}
+
+fn matcher(color:&Colors){
+    match color {
+        Colors::Red => println!("{}", "This is Red color"),
+        Colors::Green => println!("{}", "This is Green color"),
+        Colors::Blue => println!("{}", "This is Blue color"),
+        Colors::Yellow => println!("{}", "This is Yellow color"),
+        Colors::Orange => println!("{}", "This is Orange color"),
+        Colors::Purple => println!("{}", "This is Purple color"),
+
+    }
+}
+
+fn main() {
+    for color in Colors::iterator() {
+        matcher(color);
+    }
+    
+}
--- src/bin/a8.rs
+++ src/bin/a8.rs
@@ -9,4 +9,51 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+enum Flavors {
+    Sweet,
+    Hot,
+    Sevukha,
+}
+
+struct Drink {
+    flavor:Flavors,
+    value:f32,
+    name:&'static str, // mine experiment with string in structures
+}
+
+fn get_drinks_data() -> Vec<Drink> {
+    let mut vec = Vec::new();
+    vec.push(Drink{
+        flavor: Flavors::Sweet, 
+        value: 0.5, 
+        name:"Caluah",
+    });
+    vec.push(Drink{
+        flavor: Flavors::Hot, 
+        value: 1.5, 
+        name:"Absent",
+    });
+    vec.push(Drink{
+        flavor: Flavors::Sevukha, 
+        value: 0.7, 
+        name:"Vodka",
+    });
+    vec
+}
+
+fn print_drinks(vec:Vec<Drink>) {
+    for drink in vec {
+        let flavor:&'static str;
+        match drink.flavor {
+            Flavors::Sweet => flavor = "Sweet",
+            Flavors::Hot => flavor = "Hot",
+            Flavors::Sevukha => flavor = "Sevukha",
+        }
+        println!("Drink:\n name: {}\n value: {:?}\n flavor: {}\n", drink.name, drink.value, flavor);
+    }
+}
+
+fn main() {
+    let vec = get_drinks_data();
+    print_drinks(vec);
+}
